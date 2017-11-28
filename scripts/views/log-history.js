68f64e79abbcd63d85aeb34d77c8adc8211afbf0
Jacob Daniel Franklin <jacob.d.franklin@gmail.com>
2017-11-28 14:29:29 -0700

    Fixed the logic for drawing the week cells

--- scripts/views/log-history.js
+++ scripts/views/log-history.js
@@ -70,7 +70,7 @@ const LogDayHistoryView = Backbone.View.extend({
         }
     },
     padZero: function(number) {
-        if(number < 9)
+        if(number < 10)
             return '0' + number;
         else
             return number;
@@ -106,54 +106,60 @@ const LogWeekHistoryView = Backbone.View.extend({
             return map;
         }, {});
 
-        // Get daily logs
-        const week_logs = _.flatten(day_goals.map(goal => {
+        // Get weekly logs
+        const week_logs = _.flatten(week_goals.map(goal => {
             return this.collection.where({ goal_id: goal.get('id') });
         }));
 
         this.$('.week-cell').remove();
-        for(let i = 0; i <= day; i++) {
-            // Get logs for this week
+        for(let i = 1; i <= day; i++) { // Start with 1, so that we don't start on 12/31 of last year
+            // Get the current date object and logs
             curr_date = new Date(year, 0, i);
-            week_start = curr_date.getDate() - curr_date.getDay();
             date_str = `${year}-${this.padZero(curr_date.getMonth() + 1)}-${this.padZero(curr_date.getDate())}`;
-            curr_week_logs = this.getDayLogs(week_logs, date_str);
+            curr_day_logs = this.getDayLogs(week_logs, date_str);
 
-            // Calculate this day's total progress
-            week_progress = curr_week_logs.reduce((progress, log) => {
+            // If it's a new week or the first day, initialize the current week's progress
+            if(curr_date.getDay() === 0 || i === 1) {
+                week_progress = {};
+            }
+
+            // Add the logs for today to the week's total progress
+            week_progress = curr_day_logs.reduce((progress, log) => {
                 if(!progress[log.get('goal_id')])
                     progress[log.get('goal_id')] = log.get('amount');
                 else
                     progress[log.get('goal_id')] += log.get('amount');
                 return progress;
-            }, {});
+            }, week_progress);
 
-            // Calculate percentage of number of goals met
-            percent_complete = 0;
-            let goals_complete = 0;
-            for(let goal_id in goal_map) {
-                if(!goal_map.hasOwnProperty(goal_id)) continue;
-                if(!goal_map[goal_id]) throw new Error("whaaaa....");
-                if(!week_progress[goal_id]) continue;
-                if(week_progress[goal_id] >= goal_map[goal_id]) goals_complete++;
+            // If it's the end of the week or the current day, calculate percentage of goals met, and add cell
+            if(curr_date.getDay() === 6 || i === day) {
+                // Calculate percentage of number of goals met
+                percent_complete = 0;
+                let goals_complete = 0;
+                for(let goal_id in goal_map) {
+                    if(!goal_map.hasOwnProperty(goal_id)) continue;
+                    if(!goal_map[goal_id]) throw new Error("whaaaa....");
+                    if(!week_progress[goal_id]) continue;
+                    if(week_progress[goal_id] >= goal_map[goal_id]) goals_complete++;
+                }
+                percent_complete = goals_complete ? goals_complete / Object.keys(goal_map).length : 0;
+
+                // Calculate the position of the current cell
+                x = (Math.floor(i / 7) - 1) * cell_size * cell_pad;
+
+                // Assign attributes
+                cell = document.createElement("cell");
+                cell.classList = "br1 week-cell";
+                cell.style.height = cell_size + "px";
+                cell.style.width = cell_size + "px";
+                cell.style.top = y + "px";
+                cell.style.left = x + "px";
+                cell.style.background = d3.interpolateGnBu(percent_complete);
+                cell.dataset.date = curr_date;
+
+                this.$el.append(cell);
             }
-            percent_complete = goals_complete ? goals_complete / Object.keys(goal_map).length : 0;
-
-            // Calculate the position of the current cell
-            x = i % 7 === 0 ? Math.floor(i / 7) * cell_size * cell_pad : x;
-            y = ((i % 7) * cell_size) * cell_pad;
-
-            // Assign attributes
-            cell = document.createElement("cell");
-            cell.classList = "br1 week-cell";
-            cell.style.height = cell_size + "px";
-            cell.style.width = cell_size + "px";
-            cell.style.top = y + "px";
-            cell.style.left = x + "px";
-            cell.style.background = d3.interpolateGnBu(percent_complete);
-            cell.dataset.date = curr_date;
-
-            this.$el.append(cell);
         }
     },
     padZero: function(number) {
