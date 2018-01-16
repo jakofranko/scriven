ac6bc328849ecfe41e67c185a826ef184b1dbfd0
Jacob Daniel Franklin <jacob.d.franklin@gmail.com>
2018-01-16 09:55:20 -0700

    Added milestone model

--- scripts/models/milestone.js
+++ scripts/models/milestone.js
@@ -1,3 +1,15 @@
-/**
- * Created by jdfrankl on 1/15/18.
- */
+const MilestoneModel = Backbone.Model.extend({
+    defaults: {
+        id: null,
+        goal_id: null,
+        name: null
+    }
+});
+
+const MilestonesCollection = Backbone.Collection.extend({
+    url: 'http://localhost:3000/milestones',
+    model: MilestoneModel,
+    parse: function(data) {
+        return data.milestones;
+    }
+});
